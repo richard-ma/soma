c64af2b269af51ec8f2988356e2cea11c3845bb7
richard-ma <richard.ma.19850509@gmail.com>
2023-02-27 12:39:55 +0800

    重定向到order.index

--- soma/__init__.py
+++ soma/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, url_for
+from soma.blueprints import order
 
 
 app = Flask(__name__)
+app.register_blueprint(order.bp)
 
 @app.route("/")
 def index():
-    return render_template("base.html")
+    return redirect(url_for('order.index'))
