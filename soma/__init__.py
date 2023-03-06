4616ec23ccfa8e1e28a7a090e873c7de1293ed92
richard-ma <richard.ma.19850509@gmail.com>
2023-03-07 06:30:00 +0800

    fix #16 $ flask --app soma:create_app run --host=0.0.0.0 --debug

--- soma/__init__.py
+++ soma/__init__.py
@@ -2,18 +2,20 @@ from flask import Flask, redirect, url_for
 from soma.blueprints import api, order, shop, logs, stripe
 from soma.config import db
 
+def create_app(config_filename=None):
+    app = Flask(__name__)
 
-app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///soma.db"
+    # db.init(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///soma.db"
-db.init(app)
+    app.register_blueprint(api.bp)
+    app.register_blueprint(order.bp)
+    app.register_blueprint(shop.bp)
+    app.register_blueprint(stripe.bp)
+    app.register_blueprint(logs.bp)
 
-app.register_blueprint(api.bp)
-app.register_blueprint(order.bp)
-app.register_blueprint(shop.bp)
-app.register_blueprint(stripe.bp)
-app.register_blueprint(logs.bp)
+    @app.route("/")
+    def index():
+        return redirect(url_for('order.index'))
 
-@app.route("/")
-def index():
-    return redirect(url_for('order.index'))
+    return app
