fbd70ac41d61622a6bc4119680d1600e896490d2
richard-ma <richard.ma.19850509@gmail.com>
2023-03-09 14:35:44 +0800

    显示订单信息

--- soma/blueprints/order/__init__.py
+++ soma/blueprints/order/__init__.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect
+from soma.models import db, Order
 
 
 bp = Blueprint('order', __name__, url_prefix='/order', template_folder='templates')
 
 @bp.route("/")
 def index():
-    return render_template("order/index.html")
+    return redirect(url_for("order.orders"))
+
+@bp.route("/orders")
+def orders():
+    orders = db.session.execute(db.select(Order).order_by(Order.id)).scalars()
+    return render_template("order/orders.html", orders=orders)
