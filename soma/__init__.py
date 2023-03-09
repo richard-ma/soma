092e22253690c79bf07b1501d5b2c144fbeb0b94
richard-ma <richard.ma.19850509@gmail.com>
2023-03-09 11:37:49 +0800

    创建order表，并写入数据库

--- soma/__init__.py
+++ soma/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, url_for
 from soma.blueprints import api, order, shop, logs, stripe
-from soma.models import db
+from soma.models import db, migrate
 
 def create_app(config_filename=None):
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://soma:qwerty1234@localhost/soma'
     db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(api.bp)
     app.register_blueprint(order.bp)
