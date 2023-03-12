527edd4bae812a097aecff5b4412012c25b5cadc
richard-ma <richard.ma.19850509@gmail.com>
2023-03-12 12:00:47 +0800

    fix #25

--- soma/blueprints/shop/__init__.py
+++ soma/blueprints/shop/__init__.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from soma.models import db, Shop
 
 
 bp = Blueprint('shop', __name__, url_prefix='/shop', template_folder='templates')
 
 @bp.route("/")
 def index():
-    return render_template("shop/index.html")
+    return redirect(url_for("shop.shops"))
+
+@bp.route("/shops")
+def shops():
+    shops = db.session.execute(db.select(Shop).order_by(Shop.id)).scalars()
+    return render_template("shop/shops.html", shops=shops)
