e30b7037152dcce191d2fcdb5eabd64c83c269d2
richard-ma <richard.ma.19850509@gmail.com>
2023-03-12 10:35:06 +0800

    fix #24

--- soma/blueprints/stripe/__init__.py
+++ soma/blueprints/stripe/__init__.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from soma.models import db, Stripe
 
 
 bp = Blueprint('stripe', __name__, url_prefix='/stripe', template_folder='templates')
 
 @bp.route("/")
 def index():
-    return render_template("stripe/index.html")
+    return redirect(url_for("stripe.stripes"))
+
+@bp.route("/stripes")
+def stripes():
+    stripes = db.session.execute(db.select(Stripe).order_by(Stripe.id)).scalars()
+    return render_template("stripe/stripes.html", stripes=stripes)
