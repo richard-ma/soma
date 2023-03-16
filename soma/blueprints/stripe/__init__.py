ea8d1d1c9f87ea4c3189d5708a05f043b3efe4d0
richard-ma <richard.ma.19850509@gmail.com>
2023-03-16 16:18:48 +0800

    fix #11

--- soma/blueprints/stripe/__init__.py
+++ soma/blueprints/stripe/__init__.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from soma.models import db, Stripe
+from soma.forms import CreateStripeForm
 
 
 bp = Blueprint('stripe', __name__, url_prefix='/stripe', template_folder='templates')
@@ -12,3 +13,38 @@ def index():
 def stripes():
     stripes = db.session.execute(db.select(Stripe).order_by(Stripe.id.desc())).scalars()
     return render_template("stripe/list.html", stripes=stripes)
+
+@bp.route("/create", methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        stripe = Stripe(
+            url = '',
+            email = request.form['email'],
+            mode = '0',
+            pays = '1',
+            limitway = '1',
+            onemin = request.form['onemin'],
+            onemax = request.form['onemax'],
+            totalmoney = request.form['totalmoney'],
+            totalnum = request.form['totalnum'],
+            status = 1 if request.form['status'] == 'y' else 0,
+            beizhu = request.form['beizhu'],
+            updatetime = 12333,
+            scid = '',
+            lcid = '',
+            ssid = '',
+            lsid = '',
+            type = '0',
+            sid = 0,
+            lasttime = 12333,
+            admin_id = 0,
+            purl = request.form['purl']
+        )
+
+        db.session.add(stripe)
+        db.session.commit()
+        
+        return redirect(url_for('stripe.index'))
+
+    form = CreateStripeForm()
+    return render_template("stripe/create.html", form=form)
--- soma/forms.py
+++ soma/forms.py
@@ -2,7 +2,25 @@ from wtforms import Form, StringField, IntegerField, BooleanField, TextAreaField
 
 
 class CreateStripeForm(Form):
-    pass
+    # url = db.Column(db.String(255), nullable=False, comment='收款站地址')
+    email = StringField('stripe账户')
+    # mode = db.Column(db.Enum('0','1'), nullable=False, comment='账户类型')
+    # pays = db.Column(db.Enum('2','1','3'), nullable=False, default='2')
+    # limitway = db.Column(db.Enum('3','4','2','1'), nullable=False, default='4', comment='收款限定')
+    onemin = IntegerField('单笔最小金额', default=0)
+    onemax = IntegerField('单笔最大金额', default=0)
+    totalmoney = IntegerField('限定收款金额', default=2000)
+    totalnum = IntegerField('限定收款笔数', default=100)
+    status = BooleanField('启用', default=False)
+    beizhu = TextAreaField('备注')
+    purl = StringField('收款站地址', default='')
+    # scid = db.Column(db.String(255), nullable=False,)
+    # lcid = db.Column(db.String(255), nullable=False,)
+    # ssid = db.Column(db.String(255), nullable=False,)
+    # lsid = db.Column(db.String(255), nullable=False,)
+    # type = db.Column(db.Enum('0','1'), default='0')
+    # sid = db.Column(db.Integer)
+    # admin_id = db.Column(db.Integer)
 
 
 class CreateShopForm(Form):
