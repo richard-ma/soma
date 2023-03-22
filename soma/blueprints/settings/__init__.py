c95209e6f8a119f952c689be677db74324654100
richard-ma <richard.ma.19850509@gmail.com>
2023-03-22 13:09:54 +0800

    fix #37 创建汇率添加页面

--- soma/blueprints/settings/__init__.py
+++ soma/blueprints/settings/__init__.py
@@ -1,14 +1,32 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, request
 from soma.models import db, Currency
+from soma.forms import CreateCurrencyForm
 
 
 bp = Blueprint('settings', __name__, url_prefix='/settings', template_folder='templates')
 
 @bp.route("/")
 def index():
-    return redirect(url_for("settings.orders"))
+    return redirect(url_for("settings.currency_currencies"))
 
-@bp.route("/currencies")
-def currencies():
+@bp.route("/currency/currencies")
+def currency_currencies():
     currencies = db.session.execute(db.select(Currency).order_by(Currency.id.desc())).scalars()
-    return render_template("settings/currencies.html", currencies=currencies)
+    return render_template("settings/currency/currencies.html", currencies=currencies)
+
+@bp.route("/currency/create", methods=['GET', 'POST'])
+def currency_create():
+    if request.method == 'POST':
+        currency = Currency(
+            name = request.form.get('name', ''),
+            code = request.form.get('code', '').upper(),
+            value = float(request.form.get('value', 0.00))
+        )
+        
+        db.session.add(currency)
+        db.session.commit()
+
+        return redirect(url_for("settings.currency_currencies"))
+    
+    form = CreateCurrencyForm()
+    return render_template("settings/currency/create.html", form=form)
