6bded6fb37958280b412077600650ee74fe3b80b
richard-ma <richard.ma.19850509@gmail.com>
2023-03-22 20:48:23 +0800

    添加shop的api_key生成算法

--- README.md
+++ README.md
@@ -11,3 +11,11 @@
     1. 汇率设置 currency exchange rate settings
     1. 个人资料 personal settings
 1. 运行日志 logs
+1. API Key生成算法
+    1. 哈希算法
+        1. md5
+        1. sha
+    1. 哈希参数
+        1. 购物网站网址
+        1. API Key生成时的时间
+        1. 随机salt
--- soma/helpers.py
+++ soma/helpers.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from random import choice, choices
+import hashlib
 
 
 def helper_datetime_to_timestamp(dt) -> int:
@@ -14,3 +16,24 @@ def helper_status_display(status: int):
         return '已禁用'
     else:
         return '未知'
+
+def helper_generate_api_key(url: str, salt=None, salt_length=8) -> str:
+    if salt is None:
+        population = list('abcdefghijklmnopqrstuvwxyz1234567890')
+        salt = ''.join(choices(population, k=salt_length))
+    timestamp = str(helper_datetime_to_timestamp(datetime.now()))
+    params = [
+        url,
+        timestamp,
+        salt,
+    ]
+    
+    method = choice(['sha256', 'md5'])
+    hash_func = hashlib.new(method) # 选择哈希算法
+    hash_func.update(''.join(params).encode())
+    return hash_func.hexdigest()[:32]
+
+
+if __name__ == "__main__":
+    api_key = helper_generate_api_key("http://www.hello.com")
+    print(api_key)
