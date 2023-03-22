d516530f8daf78f733c056ab0c36c4c655f064b1
richard-ma <richard.ma.19850509@gmail.com>
2023-03-22 13:09:23 +0800

    添加一些TODO项目

--- soma/forms.py
+++ soma/forms.py
@@ -1,9 +1,10 @@
-from wtforms import Form, StringField, IntegerField, BooleanField, TextAreaField
+from wtforms import Form, StringField, IntegerField, BooleanField, TextAreaField, FloatField
+# 所有验证失败提示和处理
 
 
 class CreateStripeForm(Form):
     # url = db.Column(db.String(255), nullable=False, comment='收款站地址')
-    email = StringField('stripe账户')
+    email = StringField('stripe账户') # TODO 必选项
     # mode = db.Column(db.Enum('0','1'), nullable=False, comment='账户类型')
     # pays = db.Column(db.Enum('2','1','3'), nullable=False, default='2')
     # limitway = db.Column(db.Enum('3','4','2','1'), nullable=False, default='4', comment='收款限定')
@@ -13,7 +14,7 @@ class CreateStripeForm(Form):
     totalnum = IntegerField('限定收款笔数', default=100)
     status = BooleanField('启用', default=False)
     beizhu = TextAreaField('备注')
-    purl = StringField('收款站地址', default='')
+    purl = StringField('收款站地址', default='') # TODO 必选项
     # scid = db.Column(db.String(255), nullable=False,)
     # lcid = db.Column(db.String(255), nullable=False,)
     # ssid = db.Column(db.String(255), nullable=False,)
@@ -25,7 +26,7 @@ class CreateStripeForm(Form):
 
 class CreateShopForm(Form):
     status = BooleanField("启用", default=True)
-    url = StringField("网址", description="购物网站网址")
+    url = StringField("网址", description="购物网站网址") # TODO 必选项
     beizhu = TextAreaField("备注")
     # sitegroup = IntegerField("收款组")
     # type = db.Column(db.Integer, comment='网站类型')
@@ -33,3 +34,8 @@ class CreateShopForm(Form):
     # risk = db.Column(db.Integer)
     # admin_id = db.Column(db.Integer)
     # donatename = db.Column(db.String(255), default='')
+
+class CreateCurrencyForm(Form):
+    name = StringField("货币名称") # TODO 必选项
+    code = StringField("币种代码") # TODO 必选项
+    value = FloatField("汇率") # TODO 必选项
