959e1e66611df4972b1d58e9368897e65be49971
richard-ma <richard.ma.19850509@gmail.com>
2023-04-19 22:49:52 +0800

    fix #50 添加logs展示页面

--- soma/blueprints/logs/__init__.py
+++ soma/blueprints/logs/__init__.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
+from soma.models import db, Log
+from soma.helpers import *
 
 
 bp = Blueprint('logs', __name__, url_prefix='/logs', template_folder='templates')
 
 @bp.route("/")
 def index():
-    return render_template("logs/index.html")
+    return redirect(url_for('logs.logs'))
+
+@bp.route("/logs")
+def logs():
+    logs = db.session.execute(db.select(Log).order_by(Log.id.desc())).scalars()
+    return render_template('logs/list.html', logs=logs)
