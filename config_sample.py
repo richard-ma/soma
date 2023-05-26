9d19e24da147727e9efacd810ec2fdda89776082
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-26 13:34:41 +0800

    add comments

--- config_sample.py
+++ config_sample.py
@@ -3,6 +3,7 @@ class Config:
     LOGGING_FILE_MAX_BYTES = 1024 * 1024
     LOGGING_FILE_BACKUP_COUNT = 10
 
+    # get all keys and values of configuration
     def __str__(self):
         return "/".join([k+": "+v for k, v in vars(self.__class__).items() if not k.startswith('_')])
 
@@ -16,5 +17,5 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///soma-development.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///soma-testing.db' # TODO memory database
     LOGGING_FILENAME = 'testing.log'
