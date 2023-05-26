bf8cabdc0293e33a50e2c8c3ae5c4c885a9e1b83
Richard Ma <richard.ma.19850509@gmail.com>
2023-05-26 10:58:07 +0800

    add some basic config

--- config_sample.py
+++ config_sample.py
@@ -1,11 +1,20 @@
 class Config:
     TESTING = False
+    LOGGING_FILE_MAX_BYTES = 1024 * 1024
+    LOGGING_FILE_BACKUP_COUNT = 10
+
+    def __str__(self):
+        return "/".join([k+": "+v for k, v in vars(self.__class__).items() if not k.startswith('_')])
 
 class ProductionConfig(Config):
-    DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///soma-production.db'
+    LOGGING_FILENAME = 'production.log'
 
 class DevelopmentConfig(Config):
-    DATABASE_URI = ''
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///soma-development.db'
+    LOGGING_FILENAME = 'development.log'
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///soma-development.db'
+    LOGGING_FILENAME = 'testing.log'
